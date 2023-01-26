5283200f545366b7a5cd7780d5e3fd3e40855343
aamador0202 <77211255+aamador0202@users.noreply.github.com>
2023-01-26 14:41:59 -0600

    Update main.tf

--- cloud-builds/main.tf
+++ cloud-builds/main.tf
@@ -5,7 +5,7 @@ resource "google_cloudbuild_trigger" "tf-trigger" {
 
   trigger_template {
     branch_name = "master"
-    repo_name   = "https://github.com/aamador0202/timeoff-management-application"
+    repo_name   = "aamador0202/timeoff-management-application "
   }
 
   filename = "cloudbuild.yaml"
