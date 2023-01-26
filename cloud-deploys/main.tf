afef451e095b6f2c5957bdf2e47753d6c189f207
aamador0202 <77211255+aamador0202@users.noreply.github.com>
2023-01-26 12:26:19 -0600

    Create main.tf

---
+++ cloud-deploys/main.tf
@@ -0,0 +1,25 @@
+resource "google_clouddeploy_target" "primary" {
+  location = "us-central1"
+  name     = "qsdev"
+
+  description = "development cluster"
+
+  gke {
+    cluster = "projects/gorilla-375616/locations/us-central1-a/clusters/to-managment-gke"
+  }
+  project          = "gorilla-375616"
+  require_approval = false
+}
+
+resource "google_clouddeploy_delivery_pipeline" "primary" {
+  location = "us-central1"
+  name     = "time-off-app"
+  project = "gorilla-375616
+
+  serial_pipeline {
+    stages {
+      profiles  = []
+      target_id = "qsdev"
+    }
+  }
+}
